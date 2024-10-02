cdb0b3f2ab067e863e8935e8145d5ea364489200
Chelsea <cberrios5114@gmail.com>
2024-10-02 09:44:05 -0400

    Added files

--- src/app/article/[slug]/page.js
+++ src/app/article/[slug]/page.js
@@ -1,8 +1,27 @@
-
+import data from "../../data.json";
+import styles from "../../page.module.css";
+import ArticleTextDisplay from "../../component/ArticleTextDisplay";
+import { formatDateForArticle } from "../../component/utils";
 export default function Article({params}) {
+  const slug = params.slug;
+  const articleData = data.find((article) => slug === article.id);
+
     return (
       <main>
-        <h1>Article: {params.slug}</h1>
+        <div 
+        className={styles.articlePageHeaderWrapper} 
+        style={{
+          backgroundImage: `url(${articleData.image.url})`
+        }}>
+        <h1>{articleData.title}</h1>
+        <p>{formatDateForArticle(articleData.publishedDate)}</p>
+        <p className={styles.articleDayWrapper}>{articleData.blurb}</p>
+        </div>
+        <div 
+        className={styles.articleTextWrapper} >
+        {articleData.articleText?.map((text, i) => (
+        <ArticleTextDisplay key={i} data={text.data} type= {text.type} />
+        ))}</div>
         </main>
     );
   }
--- src/app/component/articleCard.js
+++ src/app/component/articleCard.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import styles from "../page.module.css"
-export default function ArticleCard({blurb, id, image, publishedDate, title}){
+import { formatDateForArticle } from "./utils"
+export default function ArticleCard({
+    blurb, id, image, publishedDate, title
+}){
     return (
         <div className={styles.articleCardWrapper}>
             <img src={image.url} alt ={image.alt} />
             <div className={styles.articleCardText}>
             <h2>{title}</h2>
-            <p className={styles.articleCardDate}>{publishedDate}</p>
+            <p className={styles.articleCardDate}>{formatDateForArticle(publishedDate)}</p>
             <p>{blurb}</p>
             <Link href ={`article/${id}`}>Read More</Link>
             </div>
